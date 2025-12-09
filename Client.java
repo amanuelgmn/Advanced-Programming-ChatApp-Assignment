94236882fef0e0bd5bd0a8a8723bb54539ab8472
amanuel <amanuelgm@gmail.com>
2025-12-09 22:51:07 +0300

    implement a simple client that can receive messages and sends message to the group chat

---
+++ Client.java
@@ -0,0 +1,35 @@
+package Java_chatApp;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost",5000);
+            System.out.println("connected to chat server.");
+
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+            //thread to listen to incoming messages
+            new Thread(() -> {
+                try {
+                    String msg;
+                    while ((msg = in.readLine()) != null){
+                        System.out.println(msg);
+                    } 
+                }catch (IOException ignored){}
+            }).start();
+
+            //main thread sends messages
+            String userInput;
+            while((userInput = inputReader.readLine()) != null ){
+                out.println(userInput);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
